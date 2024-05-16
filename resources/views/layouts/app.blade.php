87a9fb6025daba7487547f0281ff89e65df2df65
Never Lozano <nlozanob@ufpso.edu.co>
2024-05-16 01:21:23 -0500

    inicio y registro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Optica') }}</title>
+    <title>{{ config('app.name', 'Parqueadero') }}</title>
 
 
     <!-- Google Font: Source Sans Pro -->
